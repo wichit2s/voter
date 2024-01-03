c801f98d2e8b425311302fab9599411963ffa323
Wichit Sombat <wichit.s@ubu.ac.th>
2024-01-03 23:50:38 +0700

    update home app with questions page

--- Voterprj/home/urls.py
+++ Voterprj/home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'), 
+    path('questions/', views.questions, name='questions')
 ]
--- Voterprj/home/views.py
+++ Voterprj/home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from vote import models
 
 # Create your views here.
 def home(req):
     return render(req, 'home/home.html')
+
+def questions(req):
+    context = {
+            'questions': models.Question.objects.all()
+    }
+    return render(req, 'home/questions.html', context)
